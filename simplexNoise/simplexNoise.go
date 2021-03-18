3a2f07619d95724909c79e95e1d381a6c64a38fd
LucasK1 <l.kozlowski@mailbox.org>
2021-03-18 15:34:13 +0100

    More noise

--- simplexNoise/simplexNoise.go
+++ simplexNoise/simplexNoise.go
@@ -12,31 +12,89 @@ type color struct {
 	r, g, b byte
 }
 
-func setPixel(x, y int, c color, pixels []byte) {
-	index := (y*winWidth + x) * 4
-
-	if index < len(pixels)-4 && index >= 0 {
-		pixels[index] = c.r
-		pixels[index+1] = c.g
-		pixels[index+2] = c.b
-	}
+func lerp(b1, b2 byte, pct float32) byte {
+	return byte(float32(b1) + pct*(float32(b2)-float32(b1)))
 }
 
-func rescaleAndDraw(noise []float32, min, max float32, pixels []byte) {
+func colorLerp(c1, c2 color, pct float32) color {
+	return color{lerp(c1.r, c2.r, pct), lerp(c1.g, c2.g, pct), lerp(c1.b, c2.b, pct)}
+}
+
+func getGradient(c1, c2 color) []color {
+	result := make([]color, 256)
+	for i := range result {
+		pct := float32(i) / float32(255)
+		result[i] = colorLerp(c1, c2, pct)
+	}
+	return result
+}
+
+func getDualGradient(c1, c2, c3, c4 color) []color {
+	result := make([]color, 256)
+	for i := range result {
+		pct := float32(i) / float32(255)
+		if pct < 0.5 {
+			result[i] = colorLerp(c1, c2, pct*float32(2))
+		} else {
+			result[i] = colorLerp(c3, c4, pct*float32(1.5)-float32(0.5))
+		}
+	}
+	return result
+}
+
+func clamp(min, max, v int) int {
+	if v < min {
+		v = min
+	} else if v > max {
+		v = max
+	}
+	return v
+}
+
+func rescaleAndDraw(noise []float32, min, max float32, gradient []color, pixels []byte) {
 	scale := 255.0 / (max - min)
 	offset := min * scale
 
 	for i := range noise {
 		noise[i] = noise[i]*scale - offset
-		b := byte(noise[i])
 
-		pixels[i*4] = b
-		pixels[i*4+1] = b
-		pixels[i*4+2] = b
+		c := gradient[clamp(0, 255, int(noise[i]))]
+		p := i * 4
+
+		pixels[p] = c.r
+		pixels[p+1] = c.g
+		pixels[p+2] = c.b
 	}
 }
 
-func makeNoise(pixels []byte) {
+func turbulence(x, y, frequency, lacunarity, gain float32, octaves int) float32 {
+	var sum float32
+	amplitude := float32(1)
+
+	for i := 0; i < octaves; i++ {
+		f := snoise2(x*frequency, y*frequency) * amplitude
+		if f < 0 {
+			f *= -1
+		}
+		sum += f
+		frequency *= lacunarity
+		amplitude *= gain
+	}
+	return sum
+}
+
+func fbm2(x, y, frequency, lacunarity, gain float32, octaves int) float32 {
+	var sum float32
+	amplitude := float32(1)
+	for i := 0; i < octaves; i++ {
+		sum += snoise2(x*frequency, y*frequency) * amplitude
+		frequency *= lacunarity
+		amplitude *= gain
+	}
+	return sum
+}
+
+func makeNoise(pixels []byte, frequency, lacunarity, gain float32, octaves int) {
 	noise := make([]float32, winHeight*winWidth)
 
 	i := 0
@@ -44,7 +102,7 @@ func makeNoise(pixels []byte) {
 	max := float32(-9999.9)
 	for y := 0; y < winHeight; y++ {
 		for x := 0; x < winWidth; x++ {
-			noise[i] = snoise2(float32(x)/100, float32(y)/100)
+			noise[i] = turbulence(float32(x), float32(y), frequency, lacunarity, gain, octaves)
 			if noise[i] < min {
 				min = noise[i]
 			} else if noise[i] > max {
@@ -53,7 +111,9 @@ func makeNoise(pixels []byte) {
 			i++
 		}
 	}
-	rescaleAndDraw(noise, min, max, pixels)
+	// gradient := getDualGradient(color{0, 0, 175}, color{80, 160, 244}, color{12, 192, 75}, color{255, 255, 255})
+	gradient := getGradient(color{255, 0, 0}, color{255, 242, 0})
+	rescaleAndDraw(noise, min, max, gradient, pixels)
 }
 
 func main() {
@@ -88,12 +148,12 @@ func main() {
 	defer tex.Destroy()
 
 	pixels := make([]byte, winWidth*winHeight*4)
+	frequency := float32(.01)
+	gain := float32(.2)
+	lacunarity := float32(3.0)
+	octaves := 3
 
-	makeNoise(pixels)
-
-	tex.Update(nil, pixels, winWidth*4)
-	renderer.Copy(tex, nil, nil)
-	renderer.Present()
+	keyState := sdl.GetKeyboardState()
 
 	for {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
@@ -102,6 +162,32 @@ func main() {
 				return
 			}
 		}
+
+		mult := 1
+		if keyState[sdl.SCANCODE_LSHIFT] != 0 || keyState[sdl.SCANCODE_RSHIFT] != 0 {
+			mult = -1
+		}
+		if keyState[sdl.SCANCODE_O] != 0 {
+			octaves += mult
+			makeNoise(pixels, frequency, lacunarity, gain, int(octaves))
+		}
+		if keyState[sdl.SCANCODE_F] != 0 {
+			frequency += .001 * float32(mult)
+			makeNoise(pixels, frequency, lacunarity, gain, int(octaves))
+		}
+		if keyState[sdl.SCANCODE_G] != 0 {
+			gain += 0.1 * float32(mult)
+			makeNoise(pixels, frequency, lacunarity, gain, int(octaves))
+		}
+		if keyState[sdl.SCANCODE_L] != 0 {
+			lacunarity += 0.1 * float32(mult)
+			makeNoise(pixels, frequency, lacunarity, gain, int(octaves))
+		}
+
+		tex.Update(nil, pixels, winWidth*4)
+		renderer.Copy(tex, nil, nil)
+		renderer.Present()
+
 		sdl.Delay(16)
 	}
 }
