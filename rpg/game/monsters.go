abc5d16ef05f226b98e03cc4e39584d65b9cdcc2
LucasK1 <l.kozlowski@mailbox.org>
2021-04-02 23:10:09 +0200

    Action points for monsters

--- rpg/game/monsters.go
+++ rpg/game/monsters.go
@@ -9,22 +9,33 @@ type Monster struct {
 	Hp       int
 	Strength int
 	Speed    float64
+	AP       float64
 }
 
 func NewRat(pos Pos) *Monster {
-	return &Monster{pos, 'R', "Rat", 5, 5, 2}
+	return &Monster{pos, 'R', "Rat", 5, 5, 2.0, 0.0}
 }
 
 func NewSpider(pos Pos) *Monster {
-	return &Monster{pos, 'S', "Spider", 10, 10, 1}
+	return &Monster{pos, 'S', "Spider", 10, 10, 1.0, 0.0}
 }
 
 func (m *Monster) Update(level *Level) {
+	m.AP += m.Speed
 	playerPos := level.Player.Pos
+
+	apInt := int(m.AP)
+
 	positions := level.astar(m.Pos, playerPos)
-	if len(positions) > 1 {
-		fmt.Println(positions)
-		m.Move(positions[1], level)
+
+	moveIndex := 1
+	for i := 0; i < apInt; i++ {
+		if moveIndex < len(positions) {
+			fmt.Println(positions)
+			m.Move(positions[moveIndex], level)
+			moveIndex++
+			m.AP--
+		}
 	}
 }
 
